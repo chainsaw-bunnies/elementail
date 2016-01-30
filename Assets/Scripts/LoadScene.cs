f273a87b7fbb0cfb36504be0327fe6528efdd8e1
Ryan Parnas <ryan.parnas@gmail.com>
2016-01-30 10:27:29 -0600

    Initial Commit

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+	public void LoadSceneByIndex(int sceneIndex)
+	{
+		SceneManager.LoadScene(sceneIndex);
+	}
+
+	public void LoadSceneByName(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}	
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Vector3 moveDirection;
+
+	void Update () {
+		Rigidbody rb = GetComponent<Rigidbody>();
+
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			transform.position + new Vector3(0, -1f, 0))		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			rb.MovePosition(transform.position + new Vector3(0, 1f, 0));
+		}
+		else if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			rb.MovePosition(transform.position + new Vector3(-1f, 0, 0));
+		}
+		else if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			rb.MovePosition(transform.position + new Vector3(1f, 0, 0));
+		}
+
+		//moveDirection = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"), 0);
+		//		moveDirection = transform.TransformDirection(moveDirection);
+	
+		//rb.MovePosition(transform.position + moveDirection);
+
+	}
+}
